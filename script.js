d6059d330099170b3b65be1e20180984fda28a30
saranshagrawal9 <en23cs301929@medicaps.ac.in>
2026-03-27 03:02:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+// Function to append values to the display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to delete the last character
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        // Using eval() is simple for a basic calculator but be aware of security risks in real applications
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
